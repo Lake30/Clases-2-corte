946044e4bf7f71d370215af5bf843c9445d55ca3
Santiagoooo <79769829+Lake30@users.noreply.github.com>
2023-11-21 21:57:23 -0500

    Update punto.cpp

--- punto.cpp
+++ punto.cpp
@@ -1,1 +1,40 @@
+#include<iostream>
+#include<math.h>
 
+using namespace std;
+
+class Punto{
+  double lat;
+  double lon;
+  
+public:
+  Punto(){
+      lat = 0;
+      lon = 0;
+  }
+  
+  Punto(double la, double lo){
+      lat = la;
+      lon = lo;
+  }
+  
+  double calc_dist(Punto p){
+      return sqrt(pow(lat-p.lat,2) + pow(lon-p.lon,2));
+  }
+    
+  void print(){
+      cout<<lat<<", "<<lon;
+  } 
+};
+
+int main()
+{
+    Punto p;
+    Punto p2 = Punto(1,1);
+    
+    p2.print();
+    cout<<endl;
+    cout<<p.calc_dist(p2);
+    
+    return 0;
+}
